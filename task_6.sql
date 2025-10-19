321ea179ed696115fff1b50be4c12260a13dc578
izzyfgs <israelmalachy21@gmail.com>
2025-10-19 18:52:47 +0100

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -3,22 +3,11 @@
 -- ============================================================
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
-SELECT 2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'
-FROM DUAL
-WHERE NOT EXISTS (
-    SELECT 1 FROM Customers WHERE customer_id = 2
-);
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
-SELECT 3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'
-FROM DUAL
-WHERE NOT EXISTS (
-    SELECT 1 FROM Customers WHERE customer_id = 3
-);
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
-SELECT 4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.'
-FROM DUAL
-WHERE NOT EXISTS (
-    SELECT 1 FROM Customers WHERE customer_id = 4
-);
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+
